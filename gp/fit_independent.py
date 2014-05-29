66dac52ade756bdc335336590b837ba5a8a56f34
Dan F-M <danfm@nyu.edu>
2014-05-29 17:06:13 -0400

    demos

--- gp/fit_independent.py
+++ gp/fit_independent.py
@@ -7,8 +7,8 @@ __all__ = ["fit_independent"]
 
 import os
 import numpy as np
-import matplotlib.pyplot as pl
-from utils import load_data
+
+from utils import load_data, plot_results
 
 
 def fit_independent(rng=(-5, 5)):
@@ -23,24 +23,10 @@ def fit_independent(rng=(-5, 5)):
     cov = np.linalg.inv(np.dot(AT, A / yerr[:, None] ** 2))
     mu = np.dot(cov, np.dot(AT, y / yerr ** 2))
 
-    # Numerically compute the constraints in the data coordinates.
-    x0 = np.linspace(rng[0], rng[1], 5000)
-    samples = np.dot(np.vander(x0, 2),
-                     np.random.multivariate_normal(mu, cov, 1000).T)
-    mean = np.mean(samples, axis=1)
-    std = np.std(samples, axis=1)
-
     # Plot these constraints with the truth and the data points.
-    fig = pl.figure(figsize=(6, 6))
-    ax = fig.add_subplot(111)
-    ax.errorbar(x, y, yerr=yerr, fmt=".k", capsize=0)
-    ax.fill_between(x0, mean+std, mean-std, color="r", alpha=0.3)
-    ax.plot(x0, true_m*x0+true_b, "k")
-    ax.set_xlim(rng)
-    ax.set_ylim(np.array([-1, 1]) * max(np.abs(ax.get_ylim())))
-    ax.set_xlabel("$x$")
-    ax.set_ylabel("$y$")
-    ax.set_title("assuming independent uncertainties")
+    samples = np.random.multivariate_normal(mu, cov, 1000)
+    fig = plot_results(x, y, yerr, samples, truth=(true_m, true_b))
+    fig.gca().set_title("assuming independent uncertainties")
     fig.savefig(os.path.join("figures", "line_independent.png"))
 
 if __name__ == "__main__":
