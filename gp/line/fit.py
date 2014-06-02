2b7c6d53f31237210eb2d3b3b9f3ae260f189de1
Dan F-M <danfm@nyu.edu>
2014-06-02 15:19:54 -0400

    mle

---
+++ gp/line/fit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function
+
+__all__ = []
+
+import emcee
+
+
+def fit_mcmc(x, y, yerr, m0, b0, hyper0, kernel, lnlike):
+    pass
